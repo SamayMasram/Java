c796eb6aa22d356f0ff5be0bd5b4123274b161f2
Samay Masram <samaymasram1404@gmail.com>
2025-12-11 10:10:04 +0530

    NewAreaOfCircle

--- area.java
+++ area.java
@@ -1,6 +1,6 @@
 public class area {
     public static void main(String[] args) {
-        double radius = 7.5;
+        double radius = 7;
         double area = Math.PI * radius * radius;
         System.out.println("Area of the circle with radius " + radius + " is: " + area);
     }
