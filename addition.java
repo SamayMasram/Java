1d6abc061cf1488dee61bf429cfbced954f26d60
Samay Masram <samaymasram1404@gmail.com>
2025-12-11 09:58:45 +0530

    Update value v0

--- addition.java
+++
@@ -1,8 +0,0 @@
-public class addition {
-    public static void main(String[] args) {
-        int a = 15;
-        int b = 10;
-        int sum = a + b;
-        System.out.println("The sum is: " + sum);
-    }
-}
